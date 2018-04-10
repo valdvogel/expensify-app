350ed22e051fdabb35390900539d40c90e3fa126
Jose Almeida <jose@MacBook-Pro.hitronhub.home>
2018-04-09 23:47:12 -0700

    Add summary

--- src/components/ExpenseDashBoardPage.js
+++ src/components/ExpenseDashBoardPage.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
-
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashBoardPage = () => (
     <div>
+        <ExpensesSummary/>
         <ExpenseListFilters/>
         <ExpenseList/>
     </div>
